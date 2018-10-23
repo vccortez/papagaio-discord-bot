05738d51403a209146218d0ad9e91f5686f9eead
Vitor Cortez <vitor.a.cortez@gmail.com>
2018-10-23 10:12:11 -0300

    Update formatting

--- commands/rate.js
+++ commands/rate.js
@@ -2,7 +2,7 @@ exports.run = async (client, message, args) => {
   const cRate = message.settings.rate
 
   if (!args || args.length < 1) {
-    return await message.channel.send(`there is a ${cRate}% chance I reply to a message`)
+    return await message.channel.send(`there is a \`${cRate}%\` chance I reply to a message`)
   }
 
   if (message.author.id !== client.config.owner) {
@@ -13,10 +13,10 @@ exports.run = async (client, message, args) => {
 
   if (!rate || rate < 1 || rate > 50) {
     client.logger.bot(`invalid value : ${rate}`, 'rate')
-    return await message.channel.send('send me a number between 1 and 50 !')
+    return await message.channel.send('send me a number between `1` and `50` !')
   }
 
   client.settings.set(message.guild.id, rate, 'rate')
   client.logger.bot(`updated value : ${rate}`, 'rate')
-  await message.channel.send(`updated reply chance to ${rate}%`)
+  await message.channel.send(`updated reply chance to \`${rate}%\``)
 }
